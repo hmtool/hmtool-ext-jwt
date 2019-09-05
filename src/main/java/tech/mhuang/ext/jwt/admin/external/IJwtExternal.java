e465c7ff61239ac6fff6d4ac4594749918453d65
mhuang <mhuang_email@163.com>
2019-09-05 15:59:18 +0800

    提交注释！

--- src/main/java/tech/mhuang/ext/jwt/admin/external/IJwtExternal.java
+++ src/main/java/tech/mhuang/ext/jwt/admin/external/IJwtExternal.java
@@ -3,7 +3,6 @@ package tech.mhuang.ext.jwt.admin.external;
 import tech.mhuang.ext.jwt.admin.JwtHelper;
 
 /**
- *
  * jwt 扩展类
  *
  * @author mhuang
@@ -11,7 +10,12 @@ import tech.mhuang.ext.jwt.admin.JwtHelper;
  */
 public interface IJwtExternal {
 
-    default IJwtProducer create(){
+    /**
+     * 默认创建的方法，可重构
+     *
+     * @return 返回jwt生产接口
+     */
+    default IJwtProducer create() {
         return new JwtHelper();
     }
 }
--- src/main/java/tech/mhuang/ext/jwt/admin/external/IJwtProducer.java
+++ src/main/java/tech/mhuang/ext/jwt/admin/external/IJwtProducer.java
@@ -12,11 +12,34 @@ import java.util.Map;
  */
 public interface IJwtProducer {
 
+    /**
+     * 添加jwt配置
+     *
+     * @param jwt 配置
+     */
     public void add(Jwt jwt);
 
+    /**
+     * 解析token
+     *
+     * @param jsonWebToken token
+     * @return 返回解析后的map对象
+     */
     public Map<String, Object> parse(String jsonWebToken);
 
+    /**
+     * 刷新token
+     *
+     * @param claims 通过参数进行刷新
+     * @return 返回刷新后的token
+     */
     public String refresh(Map<String, Object> claims);
 
+    /**
+     * 创建token
+     *
+     * @param claims 传递需要的参数、解析后可获得
+     * @return 返回创建后的加密Token
+     */
     public String create(Map<String, Object> claims);
 }
